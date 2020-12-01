bc27b2e0df71a56fe9fabb07e739abc8ece09a9a
wecassidy <wec.notif.github@gmail.com>
2020-12-01 17:56:03 -0500

    Print a table when given a list of cases

--- dp_cite.py
+++ dp_cite.py
@@ -4,7 +4,12 @@
 Extract citations from CLST 308 practice cases
 """
 
+import argparse
 import re
+
+import docx2txt
+
+import tree
 
 CITE_RE = r"\d+(?:\.\w+)+(?:\(\w+\))*"  # Match the reasonable 8.3.2(c)(ii) and the abomination 8.3.2.c.ii
 SUBSECTION_RE = r"\b\w+\b"  # Match subsections in a citation
@@ -22,4 +27,21 @@ def citation_sections(cite):
 
     For example, `citation_sections("8.7.3(b)") -> ["8", "7", "3", "b"]`
     """
-    yield from re.findall(SUBSECTION_RE, cite)
+    return re.findall(SUBSECTION_RE, cite)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("docs", nargs="+", help="List of documents to scrape")
+    args = parser.parse_args()
+
+    dp = tree.Node("", "")
+    for doc in args.docs:
+        text = docx2txt.process(doc)
+        citations = map(citation_sections, find_citations(text))
+        for c in citations:
+            dp.add_cite(c)
+
+    print("Section\t\tDirect\tCumulative")
+    for sec in reversed(dp.flatten(tree.by_cumulative)):
+        print("{}\t\t{}\t{}".format(sec.cite, sec.individual_weight(), sec.weight))
