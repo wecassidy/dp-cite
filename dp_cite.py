2bdc65c1bc397c38fcfdcd8a217f64e6f4e088e5
wecassidy <wec.notif.github@gmail.com>
2020-12-01 16:05:43 -0500

    Extract subsections from citations

--- dp_cite.py
+++ dp_cite.py
@@ -7,10 +7,19 @@ Extract citations from CLST 308 practice cases
 import re
 
 CITE_RE = r"\d+(?:\.\w+)+(?:\(\w+\))*"  # Match the reasonable 8.3.2(c)(ii) and the abomination 8.3.2.c.ii
+SUBSECTION_RE = r"\b\w+\b"  # Match subsections in a citation
 
 
 def find_citations(text):
     """Given the text of a word document, extract citations from it (as strings)."""
-
     text = text.lower()  # normalize
     yield from re.findall(CITE_RE, text)
+
+
+def citation_sections(cite):
+    """
+    Given a string citation, process to an sequence of subsection numbers.
+
+    For example, `citation_sections("8.7.3(b)") -> ["8", "7", "3", "b"]`
+    """
+    yield from re.findall(SUBSECTION_RE, cite)
